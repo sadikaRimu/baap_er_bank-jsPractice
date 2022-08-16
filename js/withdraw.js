44e9c37bac23f60ff8432626c5430abb42566894
sadikaRimu <sadika3046@gmail.com>
2022-08-16 21:27:59 +0600

    first commit js practice

---
+++ js/withdraw.js
@@ -0,0 +1,14 @@
+//withdraw section
+//document.getElementById('btn-withdraw').addEventListener('click', function () {
+//console.log('get it');
+//const NewtWithdrawId = document.getElementById('withdraw-field');
+//const NewWithdrawString = NewtWithdrawId.value;
+// const NewWithdraw = parseFloat(NewWithdrawString);
+// const oldWithdrawId = document.getElementById('withdraw-amount');
+// const oldWithdrawString = oldWithdrawId.innerText;
+// const oldWithdraw = parseFloat(oldWithdrawString);
+// const currentWithdraw = NewWithdraw + oldWithdraw;
+// NewtWithdrawId.innerText = currentWithdraw;
+// NewtWithdrawId.value = '';
+//});
+
